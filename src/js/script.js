7a95639c4e596e6b0dfb7ab000fe8b5bc644f02a
Boris <borisnevedrov@yandex.ua>
2021-05-01 12:08:24 +0300

    courusel another way

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,37 @@
-$(document).ready(function()
+/* $(document).ready(function()
 {
-    $('.carousel__inner').slick({
-        speed: 1200,
-        /* adaptiveHeight: true, */
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icon/chevron-left-solid.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icon/chevron-right-solid.png"></button>',
-        responsive: [
-            {
-              breakpoint: 992,
-              settings: {
-                dots: true,
-                arrows: false
-              }
-            },
-            
-          ]
+	$('.carousel__inner').slick({
+		speed: 1200,
+		prevArrow: '<button type="button" class="slick-prev"><img src="icon/chevron-left-solid.png"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icon/chevron-right-solid.png"></button>',
+		responsive: [
+			{
+			  breakpoint: 992,
+			  settings: {
+				dots: true,
+				arrows: false
+			  }
+			},
+			
+		  ]
 
-      });
-});
+	  });
+}); */
+
+		const slider = tns({
+		container: '.carousel__inner',
+		items: 1,
+		slideBy: 'page',
+		autoplay: false,
+		controls: false,
+		nav: false
+		});
+
+		document.querySelector('.prev').addEventListener('click', function () {
+			slider.goTo('prev');
+		  });
+
+		document.querySelector('.next').addEventListener('click', function () {
+			slider.goTo('next');
+		});
+		  
