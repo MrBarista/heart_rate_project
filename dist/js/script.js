6c52d01d04b7f09ca0681d186296fbf6b5e5e234
Boris <borisnevedrov@yandex.ua>
2021-05-20 21:17:23 +0300

    big update: optimization, gulp and etc

---
+++ dist/js/script.js
@@ -0,0 +1,146 @@
+/* $(document).ready(function()
+{
+	$('.carousel__inner').slick({
+		speed: 1200,
+		prevArrow: '<button type="button" class="slick-prev"><img src="icon/chevron-left-solid.png"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icon/chevron-right-solid.png"></button>',
+		responsive: [
+			{
+			  breakpoint: 992,
+			  settings: {
+				dots: true,
+				arrows: false
+			  }
+			},
+			
+		  ]
+
+	  });
+}); */
+
+$(function() {
+		const slider = tns({
+		container: '.carousel__inner',
+		items: 1,
+		slideBy: 'page',
+		autoplay: false,
+		controls: false,
+		nav: false,
+		});
+
+		document.querySelector('.prev').addEventListener('click', function () {
+			slider.goTo('prev');
+		  });
+
+		document.querySelector('.next').addEventListener('click', function () {
+			slider.goTo('next');
+		});
+	});
+		
+		$(function() {
+			$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+			  $(this)
+				.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+				.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+			});
+			
+			function toggleSlide(item) {
+				$(item).each(function(i) {
+					$(this).on('click', function(e){
+						e.preventDefault();
+						$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+						$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+					})
+				})
+			};
+
+			toggleSlide('.catalog-item__link');
+			toggleSlide('.catalog-item__back');
+
+			//modal
+			
+			$('[data-modal=consultation]').on('click', function(){
+				$('.overlay, #consultation').fadeIn('slow');
+			});
+
+			$('.modal__close').on('click', function(){
+				$('.overlay, .modal').fadeOut('fast');
+			});
+
+			$('.button_mini').each(function(i) {
+				$(this).on('click', function() {
+					$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+					$('.overlay, #order').fadeIn('slow');
+				});
+			});
+
+			function valideForm(form){
+				$(form).validate({
+					rules: {
+						name:{
+							required: true,
+							  minlength: 2
+						},
+						phone:"required",
+						email: {
+							required: true,
+							email: true
+						}
+					},
+					messages: {
+						name: {
+							required: "Пожалуйста, введите своё имя",
+							minlength: jQuery.validator.format("Введите минимум {0} символа")
+						},
+						phone: "Пожалуйста, введите свой номер телефона",
+						email:{
+							required: "Пожалуйста, введите свою почту",
+							 email: "Неправильно введён адрес почты"
+						}	
+					}
+				});
+			};
+			
+			valideForm('#consultation-form');
+			valideForm('#consultation form');
+			valideForm('#order form');
+
+			$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+			$('form').submit(function(e){
+				e.preventDefault();
+				$.ajax({
+					type: "POST", // type - получить или отдать данные, post - отдать
+					url: "mailer/smart.php",
+					data: $(this).serialize()
+				}).done(function(){
+					$(this).find("input").val("");
+					$('#consultation, #order').fadeOut();
+					$('.overlay, #thanks').fadeIn();
+
+
+					$('form').trigger('reset');
+
+				});
+				return false;
+			});
+
+			//smooth scroll and pageup
+
+			$(window).scroll(function(){
+				if ($(this).scrollTop() > 1600 ){
+					$('.pageup').fadeIn();
+				} else {
+					$('.pageup').fadeOut();
+				}
+			});
+				// странно скрипт отрабатывает; вообще, и без него скролится плавно; 
+			/*  $("a[href^='#']").click(function(){ // какой то атрибут href будет начинатьься с решётки
+                const _href = $(this).attr("href");
+                $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+                return false;
+        	}); */
+
+			/// wow.min.js для красивой анимации, когда пользователь впервые видит элемент
+			new WOW().init();
+		});
